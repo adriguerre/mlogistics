7d10d2f43345fdb81b1abbe36cb2e4f09cd354d9
Adrián Guerrero Álvarez <adrianguerreroalvarez@gmail.com>
2026-03-25 16:06:01 +0100

    initial commit

---
+++ src/main/java/com/logistics/mlogistics/MlogisticsApplication.java
@@ -0,0 +1,13 @@
+package com.logistics.mlogistics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlogisticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlogisticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logistics/mlogistics/MlogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logistics.mlogistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlogisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
